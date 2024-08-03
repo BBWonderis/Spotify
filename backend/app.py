df0bb50921fcb13e91d6ce8e6afc99a4f38e10ed
BBWonderis <martiska14lukas@gmail.com>
2024-08-03 13:21:19 +0200

    creating get function

--- backend/app.py
+++ backend/app.py
@@ -1,1 +1,15 @@
-import os
+from flask import Flask, jsonify
+from config import app, db
+from models import User
+
+@app.route('/users', methods=['GET'])
+def get_users():
+    users = User.query.all()
+    json_users = list(map(lambda x: x.to_json(), users))
+    json_users.append("Lukas")
+    return jsonify({'users': json_users})
+
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
