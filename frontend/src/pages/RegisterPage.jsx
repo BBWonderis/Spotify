338597c3a1d2fdbca67f934c285fd81da0e47461
BBWonderis <martiska14lukas@gmail.com>
2024-08-03 22:46:33 +0200

    creating registerPage and styling it

--- frontend/src/pages/RegisterPage.jsx
+++ frontend/src/pages/RegisterPage.jsx
@@ -1,4 +1,37 @@
+import "../assets/styles/RegisterPage.css";
+import spotifyLogo from "../assets/styles/svgs/spotifyLogo.svg";
+import HorizontalLine from "../components/HorizontalLine";
+import LoginOptionButton from "../components/LoginOptionButton";
+import gooleLogo from "../assets/styles/svgs/googleLogo.svg";
+import appleLogo from "../assets/styles/svgs/appleLogo.svg";
+import facebookLogo from "../assets/styles/svgs/facebookLogo.svg";
+
 function RegisterPage() {
-  return <div>register hfdere</div>;
+  return (
+    <div className="register-page">
+      <div className="container">
+        <img src={spotifyLogo} alt="spotify-logo" className="spotify-logo" />
+        <h1>Register and start listening</h1>
+        <div className="input-element email">
+          <h6>E-mail</h6>
+          <input
+            type="text"
+            placeholder="E-mail"
+            className="email-input"
+            name="login"
+          />
+        </div>
+        <div className="submit">
+          <button className="submit-button">Continue</button>
+        </div>
+        <HorizontalLine />
+        <div className="login-with">
+          <LoginOptionButton name="Google" logo={gooleLogo} />
+          <LoginOptionButton name="Facebook" logo={facebookLogo} />
+          <LoginOptionButton name="Apple" logo={appleLogo} />
+        </div>
+      </div>
+    </div>
+  );
 }
 export default RegisterPage;
