5b8f416f52dd9e50ad80fb1c0c8b3a1835d097fe
BBWonderis <martiska14lukas@gmail.com>
2024-08-03 14:33:18 +0200

    create user function

--- backend/app.py
+++ backend/app.py
@@ -6,13 +6,24 @@ from models import User
 def get_users():
     users = User.query.all()
     json_users = list(map(lambda x: x.to_json(), users))
-    json_users.append("Lukas")
+
     return jsonify({'users': json_users})
 
 @app.route('/create_users', methods=['POST'])
 def create_users():
+
     data = request.get_json()
-    return jsonify(data), 201
+    username, password, email = data['username'], data['password'], data['email']
+    if not username or not password or not email:
+        return jsonify({'error': 'Username or password or email is empty'}), 400
+
+    new_user = User(username=username, password=password, email=email)
+    try:
+        db.session.add(new_user)
+        db.session.commit()
+    except Exception as e:
+        return jsonify({'error': str(e)}), 400
+    return jsonify({"message" : "User created!"}), 201
 
 if __name__ == "__main__":
     with app.app_context():
